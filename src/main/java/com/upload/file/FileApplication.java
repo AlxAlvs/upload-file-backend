300b4629fc3c0fbd9b60b416b5e59de26f889788
Alex Alves <alxalvs0121@gmail.com>
2023-09-14 14:31:06 -0300

    add initial configuration

---
+++ src/main/java/com/upload/file/FileApplication.java
@@ -0,0 +1,13 @@
+package com.upload.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/upload/file/controllers/UploadFileController.java
@@ -0,0 +1,14 @@
+package com.upload.file.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping(path = "/api/v1")
+@RequiredArgsConstructor
+public class UploadFileController {
+
+
+}
